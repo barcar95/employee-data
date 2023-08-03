f142a1f6b1c3bc651f6bfd6bd429d8bd59ca4cd0
barre <barrenac@oregonstate.edu>
2023-08-02 17:33:32 -0700

    Created connection and .env functionality

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: process.env.DB_USER,
+      // MySQL password
+      password: process.env.DB_PW,
+      database: process.env.DB_NAME
+    },
+    console.log(`Connected to the books_db database.`)
+  );
